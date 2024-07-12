42c647cbd5d17a3487c21d928311f5024df0ad70
jafar <jafarmazareei@yahoo.com>
2024-07-12 12:47:48 -0300

    git jafar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello world!");
 
         LibraryJafar.dependencyCheckAnalyze();
-        // // // // //
+        // // // // // ..//
         LibraryJafar.showReportLogCat();
     }
 }
